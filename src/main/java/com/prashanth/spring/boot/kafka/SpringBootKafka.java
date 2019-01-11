c8f309234370b99690cbc2ee457ac0c15c680928
prashanth-g <prashanthcsgce@gmail.com>
2019-01-11 20:35:12 +0530

    Added SpringBootKafka

---
+++ src/main/java/com/prashanth/spring/boot/kafka/SpringBootKafka.java
@@ -0,0 +1,11 @@
+package com.prashanth.spring.boot.kafka;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBootKafka {
+    public static void main(String[] args) {
+        SpringApplication.run(SpringBootKafka.class);
+    }
+}
