c8b30c451c82feef6763b74ae90babc1ad6a7784
prashanth-g <prashanthcsgce@gmail.com>
2019-01-14 19:27:12 +0530

    Refactored Producer

--- src/main/java/com/prashanth/spring/boot/kafka/producer/Producer.java
+++ src/main/java/com/prashanth/spring/boot/kafka/producer/Producer.java
@@ -1,8 +1,10 @@
 package com.prashanth.spring.boot.kafka.producer;
 
+import lombok.extern.java.Log;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.kafka.core.KafkaTemplate;
 
+@Log
 public class Producer {
 
     private static final String TOPIC = "channel";
@@ -11,6 +13,7 @@ public class Producer {
     private KafkaTemplate<String, String> kafkaTemplate;
 
     public void sendMessage(String message) {
+        log.info(String.format("#### -> Producing message -> %s", message));
         this.kafkaTemplate.send(TOPIC, message);
     }
 }
