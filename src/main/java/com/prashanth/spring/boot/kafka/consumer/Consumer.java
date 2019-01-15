e4b90d2996b4784aec91d45bc12b128bf8016cba
prashanth-g <prashanthcsgce@gmail.com>
2019-01-15 21:35:12 +0530

    Upated Consumer

--- src/main/java/com/prashanth/spring/boot/kafka/consumer/Consumer.java
+++ src/main/java/com/prashanth/spring/boot/kafka/consumer/Consumer.java
@@ -1,4 +1,17 @@
 package com.prashanth.spring.boot.kafka.consumer;
 
+import lombok.extern.java.Log;
+import org.springframework.kafka.annotation.KafkaListener;
+import org.springframework.stereotype.Service;
+
+import java.io.IOException;
+
+@Log
+@Service
 public class Consumer {
+
+    @KafkaListener(topics = "channels", groupId = "group_id")
+    public void consume(String message) throws IOException {
+        log.info(String.format("#### -> Consumed message -> %s", message));
+    }
 }
