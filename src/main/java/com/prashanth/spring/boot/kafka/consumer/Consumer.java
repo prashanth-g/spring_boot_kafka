f036e745b27a276508bc79c5e9f374cf60e27a88
prashanth-g <prashanthcsgce@gmail.com>
2019-01-18 19:32:43 +0530

    Fixed bean loading issue

--- src/main/java/com/prashanth/spring/boot/kafka/consumer/Consumer.java
+++ src/main/java/com/prashanth/spring/boot/kafka/consumer/Consumer.java
@@ -1,12 +1,13 @@
 package com.prashanth.spring.boot.kafka.consumer;
 
 import lombok.extern.java.Log;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.kafka.annotation.KafkaListener;
 import org.springframework.stereotype.Service;
 
 import java.io.IOException;
 
-@Log
+@Slf4j
 @Service
 public class Consumer {
 
--- src/main/java/com/prashanth/spring/boot/kafka/producer/Producer.java
+++ src/main/java/com/prashanth/spring/boot/kafka/producer/Producer.java
@@ -1,10 +1,13 @@
 package com.prashanth.spring.boot.kafka.producer;
 
 import lombok.extern.java.Log;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.kafka.core.KafkaTemplate;
+import org.springframework.stereotype.Service;
 
-@Log
+@Slf4j
+@Service
 public class Producer {
 
     private static final String TOPIC = "channel";
@@ -13,7 +16,7 @@ public class Producer {
     private KafkaTemplate<String, String> kafkaTemplate;
 
     public void sendMessage(String message) {
-        log.info(String.format("#### -> Producing message -> %s", message));
+        log.info(String.format("Producing message -> %s", message));
         this.kafkaTemplate.send(TOPIC, message);
     }
 }
