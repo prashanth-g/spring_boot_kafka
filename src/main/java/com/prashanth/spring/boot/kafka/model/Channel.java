224a8c81d5ae8fc8013c57c1bbb68c0bf682bb3b
prashanth-g <prashanthcsgce@gmail.com>
2019-01-20 17:15:24 +0530

    Added Channel Model

---
+++ src/main/java/com/prashanth/spring/boot/kafka/model/Channel.java
@@ -0,0 +1,12 @@
+package com.prashanth.spring.boot.kafka.model;
+
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter
+@Setter
+public class Channel {
+
+    private int id;
+    private String name;
+}
